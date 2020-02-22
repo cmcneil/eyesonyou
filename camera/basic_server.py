25587cdf5d01f1917c00287bd60f086105cac8d4
Carson McNeil <carson.mcneil@gmail.com>
2020-02-22 10:09:37 +0000

    Got basic websocket working

--- camera/basic_server.py
+++ camera/basic_server.py
@@ -1,14 +1,17 @@
 import asyncio
+import datetime
+import random
 import websockets
 
 
 async def hello(websocket, path):
     while True:
         now = datetime.datetime.utcnow().isoformat() + "Z"
+        print(now)
         await websocket.send("Hello at " + now)
         await asyncio.sleep(random.random() * 3)
 
-start_server = websockets.serve(hello, "127.0.0.1", 5678)
+start_server = websockets.serve(hello, '0.0.0.0', port=8888)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
